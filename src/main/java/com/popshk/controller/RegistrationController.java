1dc2256195a9dc89018b3cc5ebf5a5fc05de9e6d
popshk <dmitry.popkov.1994@gmail.com>
2019-10-08 17:48:04 +0300

    add filter for registration.

--- src/main/java/com/popshk/controller/RegistrationController.java
+++ src/main/java/com/popshk/controller/RegistrationController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/registration")
@@ -20,12 +21,16 @@ public class RegistrationController {
     public String registration(){ return "registration";}
 
     @PostMapping
-    public String addUser(User user, Model model){
+    public String addUser(User user, Model model, @RequestParam String username){
 
         User userFromDb=userRepository.findByUsername(user.getUsername());
 
         if (userFromDb != null){
             model.addAttribute("message","User exists!");
+            if (username.equals(userFromDb))
+                model.addAttribute("message","User exist!");
+            if (username.isEmpty() || username.length()<3)
+                model.addAttribute("message","Invalid User Name!");
             return "registration";
         }
 
--- src/main/resources/templates/parts/common.ftl
+++ src/main/resources/templates/parts/common.ftl
@@ -1,6 +1,7 @@
 <#macro page>
     <!DOCTYPE html>
 <html lang="en">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256 CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <head>
     <meta charset="UTF-8">
     <title>Chat</title>
